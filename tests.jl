fd7fafb4a6769d2242fc6f17d9dc9e25e6276724
kemalrose <30701894+kemalrose@users.noreply.github.com>
2021-10-29 14:23:31 +0200

    Delete a file

--- tests.jl
+++ tests.jl
@@ -2,7 +2,27 @@
 using Test
 
 
-@testset "EDdegree_examples" begin
+
+
+
+
+@testset "utilities" begin
+    A=[0 1 0 0 1 1 0 1 1 1 1 1;
+       0 1 1 1 0 0 1 0 1 1 1 1;
+       1 0 0 1 1 0 1 1 0 1 1 1;
+       1 0 1 0 0 1 1 1 1 0 1 1;
+       0 0 1 1 1 1 1 1 1 1 0 1]
+
+    @test ishomogeneous(A) == false
+    @test ishomogeneous(homogenize(A)) == true
+
+    B = reparametrise(A)
+    @test rank(A) == rank(B)
+end
+
+
+
+@testset "EDdistance" begin
     # Jux Cantor example
     A=[0 1 0 0 1 1 0 1 1 1 1 1;
        0 1 1 1 0 0 1 0 1 1 1 1;
@@ -16,22 +36,22 @@ using Test
     @test distance < 2
 
     #Octahedron example
-    A = [1 1 1 0 0 0;
+    B = [1 1 1 0 0 0;
          1 0 0 1 1 0;
          0 1 0 1 0 1;
          0 0 1 0 1 1]
-    @test EDdeg(A) == 28
-    @test EDdeg(A, true) == 28
+    @test EDdeg(B) == 28
+    @test EDdeg(B, true) == 28
 end
 
-@testset "Graph_models" begin
+@testset "Graph_model" begin
     n_verts = 7
     edges = [[1,2], [1,6], [2,3], [3,4], [4,6], [5,6], [5,7], [6,7]]
     colour = [1,1,2,2,3,3,3]
     n_colours = 3
     G = Graph(n_verts, edges, colour, n_colours)
-    A = homogenize(binom_mod(G))
-    @test EDdeg(A) == 76
-    B = homogenize(β_mod(G))
-    @test EDdeg(B) == 1
+    A = binom_mod(G)
+    #@test EDdeg(A) == 76
+    B = β_mod(G)
+    #@test EDdeg(B) == 1
 end
